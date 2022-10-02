83395e186d81e66f64d9c9456a8b38be420b20f0
prabhat1001 <prabhatsaraswat24@gmail.com>
2022-10-02 18:26:02 +0530

    feat: Event page couldn't complete on time

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import styled from 'styled-components';
 
 const Event = () => {
   return (
-    <div>Event</div>
+    <GIF src="./img/work.gif"></GIF>
   )
 }
+const GIF = styled.img`
+    width: 30%;
+    height: 30%;
+    margin-top: 10%;
+    margin-left: 35%;
+    /* background-color: #000; */
+`;
 
 export default Event
